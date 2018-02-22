bdf096f9c7bee6c12a220724adf6fa1d558648ba
Beatriz Pacheco Pinero <bpacheco@alpha27.aulas.gsyc.urjc.es>
2018-02-22 14:06:37 +0100

    avance

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,3 +1,20 @@
+function getValues(){
+	carousel();
+	folder = document.getElementById("imageType").src.split("/");
+	folder = folder[folder.length-1].split(".")[0]
+	winning = [folder + "1", folder + "2", folder + "3", folder + "4", folder + "5",
+						 folder + "6", folder + "7", folder + "8", "black"];
+	randomimage();
+	for(i=1; i<10; i++){
+		pos = "pos" + i;
+		img = document.getElementById(pos);
+		images.push(img.src);
+	};
+	carouselInterval = setInterval(carousel, 8000); // Change image every 8 seconds
+	countingInterval = setInterval(start_counting, 1000);
+	document.addEventListener("keydown", show ,false);
+};
+
 function randomimage(){
 	var type = document.getElementById("imageType").src;
 	var rand = "pos"
